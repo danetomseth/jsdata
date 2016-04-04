1daac8e496e420661c39c12fccae403fe09fe784
danetomseth <danetomseth@gmail.com>
2016-04-04 17:27:55 -0400

    services

--- public/js/services/post.service.js
+++ public/js/services/post.service.js
@@ -1,6 +1,6 @@
 'use strict';
 
-app.factory('Post', function(DS) {
+app.factory('Post', function(DS, $state) {
 
 	/*
 
@@ -13,7 +13,29 @@ app.factory('Post', function(DS) {
 				(HINT: see post.js)
 
 	*/
+	var Post = DS.defineResource({
+  		name: 'posts',
+  		relations: {
+  			belongTo: {
+  				users: {
+  					localKey: 'author'
+  					localField: '_author'
+  				}
+  			}
+  		},
+  		methods: {
+  			go: function() {
+  				$state.go('post', {
+  					postId: this._id,
+  					authorId: this.author
+  				})
+  			}
+  		}
 
-});
+  	});
+
+  	return Post;
+
+}).run(function (Post){})
 
 
